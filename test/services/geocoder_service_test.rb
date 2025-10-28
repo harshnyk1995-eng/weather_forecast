af404d69d4986a7f39976c61fe3d43efb45e5589
harshanaik <harshnyk1995@gmail.com>
2025-10-28 10:47:07 +0530

    test case changes using stub

--- test/services/geocoder_service_test.rb
+++ test/services/geocoder_service_test.rb
@@ -2,24 +2,19 @@ require "test_helper"
 
 class GeocodeServiceTest < ActiveSupport::TestCase
   test "call with known address" do
-    # Save original search method
-    original_search = Geocoder.method(:search)
-
-    # Stub Geocoder.search temporarily
-    Geocoder.define_singleton_method(:search) do |_address|
-      [ Struct.new(:latitude, :longitude, :country_code, :postal_code).new(
+    fake_result = [
+      Struct.new(:latitude, :longitude, :country_code, :postal_code).new(
         12.97, 77.59, "IN", "560001"
-      ) ]
+      )
+    ]
+
+    Geocoder.stub :search, fake_result do
+      result = GeocodeService.call("Bangalore")
+
+      assert_equal 12.97, result[:latitude]
+      assert_equal 77.59, result[:longitude]
+      assert_equal "IN", result[:country_code]
+      assert_equal "560001", result[:postal_code]
     end
-
-    result = GeocodeService.call("Bangalore")
-
-    assert_equal 12.97, result[:latitude]
-    assert_equal 77.59, result[:longitude]
-    assert_equal "IN", result[:country_code]
-    assert_equal "560001", result[:postal_code]
-  ensure
-    # Restore original method
-    Geocoder.define_singleton_method(:search, original_search)
   end
 end
